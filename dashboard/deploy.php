771dac2b907e05fcb16335e2202b09930900ec4a
Eklas Mahmud <kh.Eklas502@gmail.com>
2025-08-26 19:26:03 +0600

    deploy

--- dashboard/deploy.php
+++ dashboard/deploy.php
@@ -4,18 +4,18 @@ $payload = file_get_contents('php://input');
 $headers = getallheaders();
 
 if (!isset($headers['X-Hub-Signature-256'])) {
-    http_response_code(403);
-    die("No signature");
+  http_response_code(403);
+  die("No signature");
 }
 
 $signature = "sha256=" . hash_hmac('sha256', $payload, $secret);
 
 if (!hash_equals($signature, $headers['X-Hub-Signature-256'])) {
-    http_response_code(403);
-    die("Invalid signature");
+  http_response_code(403);
+  die("Invalid signature");
 }
 
-// Deploy
+// Deploy command
 $cmd = "
 cd /home/asadvanc/public_html/dashboard &&
 git fetch --all &&
@@ -24,7 +24,6 @@ git reset --hard origin/main 2>&1
 $output = shell_exec($cmd);
 
 // Logging
-file_put_contents("/home/asadvanc/deploy.log", date('Y-m-d H:i:s') . \"\\n\" . $output . \"\\n---\\n\", FILE_APPEND);
+file_put_contents("/home/asadvanc/deploy.log", date('Y-m-d H:i:s') . "\n" . $output . "\n---\n", FILE_APPEND);
 
-echo \"<pre>$output</pre>\";
-?>
+echo "<pre>$output</pre>";
